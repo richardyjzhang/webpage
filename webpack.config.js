896b6b70c8a65298289c1627b5533040d15c2dab
zhangyj <richard.yujia.zhang@gmail.com>
2019-03-17 12:30:01 +0800

    Babel相关配置

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,26 @@ const bundlePath = path.resolve(__dirname, 'dist/');
 
 module.exports = {
   entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            '@babel/preset-env',
+            '@babel/preset-react',
+          ],
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  resolve: { extensions: ['*', '.js', '.jsx'] },
   output: {
     publicPath: bundlePath,
     filename: 'bundle.js',
