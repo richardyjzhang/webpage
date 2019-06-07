10ca79e7f61fa7ddb4a54fa0787668b01e17e582
zhangyj <richard.yujia.zhang@gmail.com>
2019-06-07 14:37:53 +0800

    放弃使用material-ui，太不习惯了。。。

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import MyDrawer from '../component/MyDrawer';
 
 export default class IndexPage extends React.Component {
 
   render() {
     return (
-      <MyDrawer />
+      <div />
     );
   }
 }
