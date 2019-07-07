c8531137285096c32794c7997f4c95828e03a683
ZhangYJ <richard.yujia.zhang@gmail.com>
2019-07-07 16:41:17 +0800

    仿照antd封装umi-request

---
+++ src/common/baseUrl.ts
@@ -0,0 +1,25 @@
+function getByEnv(): string {
+  let url = '';
+
+  const env = process.env.API_ENV;
+
+  switch (env) {
+    case 'dev': {
+      url = 'http://127.0.0.1/api/v1/';
+      break;
+    }
+    case 'prod': {
+      url = 'https://www.zhangrichard.com/api/v1/';
+      break;
+    }
+    default: {
+      url = 'http://127.0.0.1/api/v1/';
+      break;
+    }
+  }
+
+  return url;
+};
+
+const baseURL = getByEnv();
+export default baseURL;
---
+++ src/common/request.ts
@@ -0,0 +1,22 @@
+/**
+ * request 网络请求工具
+ * 更详细的 api 文档: https://github.com/umijs/umi-request
+ */
+import { extend } from 'umi-request';
+
+/**
+ * 异常处理程序
+ */
+const errorHandler = (_: { response: Response }): void => {
+
+};
+
+/**
+ * 配置request请求时的默认参数
+ */
+const request = extend({
+  errorHandler, // 默认错误处理
+  credentials: 'include', // 默认请求是否带上cookie
+});
+
+export default request;
