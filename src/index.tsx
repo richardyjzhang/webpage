3ea37690f35c24a17931c55157e4d0ff4c76dd1d
zhangyj <richard.yujia.zhang@gmail.com>
2021-05-29 17:21:22 +0800

    首页的AppBar及Drawer及抽屉切换

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from "react-router-dom";
-import HomePage from './pages/HomePage';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#ffffff",
+    },
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
     {
-    <Router>
-        <Switch>
-          <Route path="/">
-            <HomePage />
-          </Route>
-        </Switch>
-    </Router>
+      <ThemeProvider theme={theme}>
+        <Router>
+          <Switch>
+            <Route path="/">
+              <HomePage />
+            </Route>
+          </Switch>
+        </Router>
+      </ThemeProvider>
     }
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,7 +1,105 @@
-import React from 'react';
+import React, { useState } from "react";
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
+import {
+  Drawer,
+  AppBar,
+  Toolbar,
+  CssBaseline,
+  Typography,
+  Button,
+  IconButton,
+} from "@material-ui/core";
+import { ChevronRight, ChevronLeft, CheckRounded } from "@material-ui/icons";
+import clsx from "clsx";
+
+// 左侧抽屉宽度
+const drawerWidth = 180;
+
+// 标题栏高度
+const toolBarHeight = 64;
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      display: "flex",
+    },
+    appBar: {
+      zIndex: theme.zIndex.drawer + 1,
+      boxShadow: "0 1px 2px 0 rgba(0, 0, 0, 0.12)",
+    },
+    toolBar: {
+      height: toolBarHeight,
+    },
+    drawer: {
+      flexShrink: 0,
+    },
+    drawerOpen: {
+      width: drawerWidth,
+      transition: theme.transitions.create(["width"], {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+      }),
+    },
+    drawerClose: {
+      width: theme.spacing(8),
+      transition: theme.transitions.create(["width"], {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+      }),
+      overflowX: "hidden",
+    },
+    drawerContainer: {
+      overflow: "auto",
+      height: `calc(100% - ${toolBarHeight}px)`,
+    },
+    drawerFooter: {
+      position: "absolute",
+      bottom: theme.spacing(1),
+      left: theme.spacing(1),
+    },
+  })
+);
 
 const HomePage = () => {
-    return <div>Hello, World!</div>
-}
+  const classes = useStyles();
+
+  const [drawerOpen, setDrawerOpen] = useState(false);
+
+  return (
+    <div>
+      <CssBaseline />
+      <AppBar className={classes.appBar} position="fixed">
+        <Toolbar className={classes.toolBar}>
+          <Typography variant="h6">HOMEPAGE</Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        className={clsx(classes.drawer, {
+          [classes.drawerOpen]: drawerOpen,
+          [classes.drawerClose]: !drawerOpen,
+        })}
+        classes={{
+          paper: clsx(classes.drawer, {
+            [classes.drawerOpen]: drawerOpen,
+            [classes.drawerClose]: !drawerOpen,
+          }),
+        }}
+        variant="permanent"
+      >
+        <Toolbar className={classes.toolBar} />
+        <div className={classes.drawerContainer}>
+          <IconButton
+            className={classes.drawerFooter}
+            onClick={() => {
+              setDrawerOpen(!drawerOpen);
+            }}
+          >
+            {drawerOpen ? <ChevronLeft /> : <ChevronRight />}
+          </IconButton>
+        </div>
+      </Drawer>
+    </div>
+  );
+};
 
 export default HomePage;
