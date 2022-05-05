7488e9e10a7a175f846992ab1b10688080a98636
ZhangYJ <richard.yujia.zhang@gmail.com>
2022-05-05 21:44:19 +0800

    读取配置文件

---
+++ backend/config.py
@@ -0,0 +1,19 @@
+import json
+import os
+
+if not os.path.exists('config.json'):
+    print('config.json not found!')
+    exit()
+
+fs = open('config.json', 'r')
+jsonConfig = json.load(fs)
+
+try:
+    dbhost = jsonConfig['dbhost']
+    dbuser = jsonConfig['dbuser']
+    dbpass = jsonConfig['dbpass']
+except KeyError:
+    print('config.json error!')
+    exit()
+finally :
+    fs.close()
