f143dbc51138cbacb00adb80d2b2e43f39c92d34
zhangyj <richard.yujia.zhang@gmail.com>
2023-05-31 17:23:19 +0800

    feat: 动态请求接口地址

--- frontend/src/utils/baseURL.ts
+++ frontend/src/utils/baseURL.ts
@@ -1,2 +1,4 @@
-const baseURL = "http://127.0.0.1:3001/api";
+// 动态设定协议、主机名和端口，便于部署
+// 本地开发时需要调整
+const baseURL = `${document.location.protocol}//${document.location.hostname}:${document.location.port}/api`;
 export default baseURL;
